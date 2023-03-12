3fe26a23fc9a8e5d0b4e27407be06097edfffb58
Husk <68003176+HUSK-321@users.noreply.github.com>
2023-03-12 20:42:28 +0900

    feat : 3인칭 템플릿 프로젝트 생성

---
+++ ProjectFA/Source/ProjectFA/ProjectFAGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectFAGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectFAGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectFAGameMode();
+};
+
+
+
