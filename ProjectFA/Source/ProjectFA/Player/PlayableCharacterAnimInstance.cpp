03127ac0cf3e720cc7630ec32577fb2f5366f33b
Husk <68003176+HUSK-321@users.noreply.github.com>
2023-03-12 22:37:14 +0900

    feat : 플레이어 기본 애니메이션 추가 (움직이기, 점프)

---
+++ ProjectFA/Source/ProjectFA/Player/PlayableCharacterAnimInstance.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayableCharacterAnimInstance.h"
+#include "PlayableCharacter.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+void UPlayableCharacterAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+
+	PlayableCharacter = Cast<APlayableCharacter>(TryGetPawnOwner());
+}
+
+void UPlayableCharacterAnimInstance::NativeUpdateAnimation(float DeltaSeconds)
+{
+	Super::NativeUpdateAnimation(DeltaSeconds);
+
+	SetPlayableCharacterProperties();
+}
+
+void UPlayableCharacterAnimInstance::SetPlayableCharacterProperties()
+{
+	if(PlayableCharacter == nullptr)	return;
+	auto PlayerVelocity = PlayableCharacter->GetVelocity();
+	PlayerVelocity.Z = 0.f;
+
+	Speed = PlayerVelocity.Size();
+	bIsInAir = PlayableCharacter->GetCharacterMovement()->IsFalling();
+	bIsCrouching = PlayableCharacter->bIsCrouched;
+}
---
+++ ProjectFA/Source/ProjectFA/Player/PlayableCharacterAnimInstance.h
@@ -0,0 +1,35 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "PlayableCharacterAnimInstance.generated.h"
+
+class APlayableCharacter;
+UCLASS()
+class PROJECTFA_API UPlayableCharacterAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+private:
+
+	void SetPlayableCharacterProperties();
+
+private:
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Movement", meta = (AllowPrivateAccess = "true"))
+	TObjectPtr<APlayableCharacter> PlayableCharacter;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Player Movement", meta = (AllowPrivateAccess = "true"))
+	float Speed;
+	UPROPERTY(BlueprintReadOnly, Category = "Player Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsInAir;
+	UPROPERTY(BlueprintReadOnly, Category = "Player Movement", meta = (AllowPrivateAccess = "true"))
+	bool bIsCrouching;	
+};
