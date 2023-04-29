20c21d478419775071b796dbfe520f27f9500984
Husk <68003176+HUSK-321@users.noreply.github.com>
2023-04-29 20:39:51 +0900

    chore : TODO

--- ProjectFA/Source/ProjectFA/FACharacter/Enemy/Enemy.cpp
+++ ProjectFA/Source/ProjectFA/FACharacter/Enemy/Enemy.cpp
@@ -67,6 +67,7 @@ void AEnemy::OnSensingPawn(APawn* OtherPawn)
 void AEnemy::AttackSphereOnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	// TODO : refactor not to refer APlayableCharacter
 	const auto PlayableCharacter = Cast<APlayableCharacter>(OtherActor);
 	if(PlayableCharacter == nullptr)	return;
 
