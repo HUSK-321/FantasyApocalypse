8e0906cbbfa8830228656137b3e7d5e72c0310b0
husk <huskgit@gmail.com>
2023-05-25 18:02:53 +0900

    feat : 폭탄 뿌리는 노티파이 추가

---
+++ ProjectFA/Source/ProjectFA/SkillNotifies/DeployBombAnimNotify.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "DeployBombAnimNotify.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTFA_API UDeployBombAnimNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+	
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true"))
+	TSubclassOf<AActor> DeployObjectClass;
+};
